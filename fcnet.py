18e9173bc98993376ff215d10e3def99710ea6b8
giaanthunder <giaan086.nvktd@gmail.com>
2019-03-21 09:01:39 +0700

    Add files via upload

---
+++ fcnet.py
@@ -0,0 +1,22 @@
+import time
+import numpy as np
+import matplotlib.pyplot as plt
+from cs231n.classifiers.fc_net import *
+from cs231n.data_utils import get_CIFAR10_data
+from cs231n.gradient_check import eval_numerical_gradient, eval_numerical_gradient_array
+
+plt.rcParams['figure.figsize'] = (10.0, 8.0)
+plt.rcParams['image.interpolation'] = 'nearest'
+plt.rcParams['image.cmap'] = 'gray'
+
+
+def rel_error(x, y):
+   return np.max( np.abs(x-y) / np.maximum(1e-8, np.abs(x)+np.abs(y)) )
+
+
+
+
+
+
+
+
